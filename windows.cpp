aa31d751b94783f9753a0661f92cc7daedd29b02
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-04 01:45:44 +0100

    windows: maybe that is all
    but it doesnt work

--- windows.cpp
+++ windows.cpp
@@ -19,8 +19,7 @@ class spawn_prog : public prog {
   handle m_iws;
   hpcon m_hpcon;
 
-public:
-  spawn_prog(const spawn_params &p) {
+  void create_pty(const spawn_params &p) {
     handle irs;
     handle ows;
 
@@ -48,8 +47,67 @@ public:
     }
     m_hpcon = hpcon{pc};
   }
-  void send(jute::view chars) override {}
-  void recv(buffer *b) override {}
+  auto create_startupinfo() {
+    // TODO: check for errors
+    STARTUPINFOEX si{};
+    si.StartupInfo.cb = sizeof(STARTUPINFOEX);
+
+    size_t req;
+    InitializeProcThreadAttributeList(nullptr, 1, 0, &req);
+    si.lpAttributeList = static_cast<PPROC_THREAD_ATTRIBUTE_LIST>(
+        HeapAlloc(GetProcessHeap(), 0, req));
+    InitializeProcThreadAttributeList(si.lpAttributeList, 1, 0, &req);
+
+    UpdateProcThreadAttribute(si.lpAttributeList, 0,
+                              PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, *m_hpcon,
+                              sizeof(*m_hpcon), nullptr, nullptr);
+    return si;
+  }
+
+public:
+  explicit spawn_prog(const spawn_params &p) {
+    create_pty(p);
+
+    auto si = create_startupinfo();
+
+    jute::heap args{};
+    for (auto arg : p.args) {
+      args = args + arg;
+    }
+
+    auto args_sz = (*args).size() + 1;
+    auto cli = (*args).cstr();
+
+    PROCESS_INFORMATION pi{};
+    if (!CreateProcess(nullptr, cli.data(), nullptr, nullptr, false,
+                       EXTENDED_STARTUPINFO_PRESENT, nullptr, nullptr,
+                       &si.StartupInfo, &pi)) {
+      silog::log(silog::error, "failed to create process");
+      throw spawn_failed{};
+    }
+  }
+
+  void send(jute::view chars) override {
+    DWORD wr{};
+    WriteFile(*m_iws, chars.data(), chars.size(), &wr, nullptr);
+    if (wr != chars.size()) {
+      silog::log(silog::error, "write: truncated output of %ld bytes", wr);
+      throw send_failed{};
+    }
+  }
+  void recv(buffer *b) override {
+    DWORD rd{};
+    PeekNamedPipe(*m_ors, nullptr, 0, nullptr, &rd, nullptr);
+    if (rd == 0) {
+      b->set_size(0);
+      return;
+    }
+    auto sz = (rd < b->capacity()) ? rd : b->capacity();
+
+    // TODO: check errors
+    ReadFile(*m_ors, b->data(), sz, &rd, nullptr);
+    b->set_size(sz);
+  }
 };
 
 [[nodiscard]] hai::uptr<prog> spawn(const spawn_params &p) {
